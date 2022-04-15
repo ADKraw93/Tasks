271bacb0b53f89fd6f7478146b63824a409af2f7
Artur Krawczyk <artur.damian.krawczyk@gmail.com>
2022-04-15 13:33:34 +0200

    Task 22.3 added

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,14 +1,9 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.Task;
-import com.crud.tasks.domain.TaskDto;
-import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.*;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -25,14 +20,27 @@ public class TrelloController {
     @GetMapping("boards")
     public void getBoards() {
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-        Pattern kodillaPattern = Pattern.compile("Kodilla");
+        /*Pattern kodillaPattern = Pattern.compile("Kodilla");
 
         trelloBoards.stream()
                 .filter(t -> t.getId()!=null)
                 .filter(t -> t.getName()!=null)
-                .filter(t -> kodillaPattern.matcher(t.getName()).find()) //t -> t.getName().kodillaPattern.asPredicate()
+                .filter(t -> kodillaPattern.matcher(t.getName()).find())
                 .forEach(trelloBoardDto -> {
             System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+        });*/
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloList -> {
+                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+            });
+            System.out.println("");
         });
+    }
+
+    @PostMapping("cards")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloClient.createNewCard(trelloCardDto);
     }
 }
